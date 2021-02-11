39cbb1bf70d1ea80e9128b51bac25b9573cd0eac
ifyoga <ifyoga@qmail.id>
2021-02-11 16:05:20 +0700

    commit auto

---
+++ cypress/integration/examples/mizan.specs.js
@@ -0,0 +1,9 @@
+describe('visit mizan ',()=>{
+    it('mizan is available',()=>{
+        cy.visit('https://seller.mizanmu.id/')
+        cy.get('.account-login > [href="https://seller.mizanmu.id/login"]').scrollIntoView().click({force: true})
+        cy.get('[type="email"]').type('ifyoga@qmail.id')
+        cy.get('#pass').type('haduh')
+        cy.get('.btn').should('contain','Login').click()
+    })
+})
