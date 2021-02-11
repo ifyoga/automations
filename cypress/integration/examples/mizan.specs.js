667a6b35dbd47c1361546643cd76b1ecec679f27
ifyoga <ifyoga@qmail.id>
2021-02-11 21:55:45 +0700

    initial

--- cypress/integration/examples/mizan.specs.js
+++ cypress/integration/examples/mizan.specs.js
@@ -1,5 +1,15 @@
 describe('visit mizan',()=>{
-    it('mizan is available',()=>{
+
+    it('when password is wrong',()=>{
+        cy.visit('https://seller.mizanmu.id/')
+        cy.get('.account-login > [href="https://seller.mizanmu.id/login"]').scrollIntoView().click({force: true})
+        cy.get('[type="email"]').type('ifyoga@qmail.id')
+        cy.get('#pass').type('aduh')
+        cy.get('.btn').should('contain','Login').click()
+       
+    })
+    cy.wait(4000)
+    it('Green action',()=>{
         cy.visit('https://seller.mizanmu.id/')
         cy.get('.account-login > [href="https://seller.mizanmu.id/login"]').scrollIntoView().click({force: true})
         cy.get('[type="email"]').type('ifyoga@qmail.id')
@@ -12,12 +22,5 @@ describe('visit mizan',()=>{
         cy.get('.visible-md > .image').click()
     })
 
-    it('error mizan login',()=>{
-        cy.visit('https://seller.mizanmu.id/')
-        cy.get('.account-login > [href="https://seller.mizanmu.id/login"]').scrollIntoView().click({force: true})
-        cy.get('[type="email"]').type('ifyoga@qmail.id')
-        cy.get('#pass').type('aduh')
-        cy.get('.btn').should('contain','Login').click()
-       
-    })
+  
 })
