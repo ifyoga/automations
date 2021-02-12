d1931179150a02e5b5d4c0f5da518a2920176bcb
ifyoga <ifyoga@qmail.id>
2021-02-12 09:41:41 +0700

    initial

--- cypress/integration/examples/mizan.specs.js
+++ cypress/integration/examples/mizan.specs.js
@@ -1,6 +1,6 @@
 describe('visit mizan',()=>{
 
-    it('when password is wrong',()=>{
+    it('when password is wrongs',()=>{
         cy.visit('https://seller.mizanmu.id/')
         cy.get('.account-login > [href="https://seller.mizanmu.id/login"]').scrollIntoView().click({force: true})
         cy.get('[type="email"]').type('ifyoga@qmail.id')
